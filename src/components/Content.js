348d70601dad2bf1ef698a4cff39c3c9b2b25ce4
helderdsa <helder_dsa@hotmail.com>
2022-07-07 19:38:53 -0300

    Requisito 5

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Content extends React.Component {
   constructor() {
     super();
     this.state = {
       products: [],
+      categories: [],
     };
   }
 
+  async componentDidMount() {
+    const response = await getCategories();
+    this.setState({
+      categories: response,
+    });
+  }
+
   render() {
-    const { products } = this.state;
+    const { products, categories } = this.state;
     return (
       <section>
         {products.length === 0 && (
@@ -18,7 +27,22 @@ class Content extends React.Component {
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>)}
         <input placeholder="Pesquisar" />
-        <Link to="/cart" data-testid="shopping-cart-button" />
+        <Link to="/cart" data-testid="shopping-cart-button">
+          <img width="32px" src="https://cdn-icons-png.flaticon.com/512/34/34627.png" alt="button" />
+        </Link>
+        <div>
+          {categories.map((categorie) => (
+            <label
+              data-testid="category"
+              className="categorie-element"
+              key={ categorie.id }
+              htmlFor={ categorie.id }
+            >
+              {categorie.name}
+              <input id={ categorie.id } type="radio" name="category-group" />
+            </label>
+          ))}
+        </div>
       </section>
     );
   }
